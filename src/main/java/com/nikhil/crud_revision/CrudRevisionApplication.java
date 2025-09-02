3f015d8efe79fdb4cb5afbdb73aeba9e96450936
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-09-02 18:56:08 +0530

    Initial set-up

---
+++ src/main/java/com/nikhil/crud_revision/CrudRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.crud_revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRevisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRevisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikhil/crud_revision/CrudRevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.crud_revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRevisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
